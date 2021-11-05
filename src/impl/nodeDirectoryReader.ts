5a524d066fc32b08f69fc052c63c27eed7c9b342
Omar Othman <omar.othman@live.com>
2021-11-05 22:55:23 +0300

    Pass defined default cwd to glob to avoid undefined error

--- src/impl/nodeDirectoryReader.ts
+++ src/impl/nodeDirectoryReader.ts
@@ -3,7 +3,7 @@ import DirectoryReader from '../core/directoryReader';
 
 export default class NodeDirectoryReader implements DirectoryReader {
 
-  read(globPatterns: string[], cwd?: string): string[] {
+  read(globPatterns: string[], cwd = '.'): string[] {
     return globPatterns.reduce((prev, cur) => {
       prev.push(...readDirectory(cur, { realpath: true, cwd }));
       return prev;
